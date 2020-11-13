05e4d43f87091764309fa291d713158ce3de3028
Juan Camilo Guzman <jcamiloguz1@gmail.com>
2020-11-12 19:04:36 -0500

    draw points in image

--- detectUNO.py
+++ detectUNO.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('9R.jpeg', cv2.IMREAD_UNCHANGED)
+img = cv2.imread('C3.JPG', cv2.IMREAD_UNCHANGED)
 
 # convert img to grey
 img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -19,14 +19,26 @@ ret, thresh_img = cv2.threshold(blur, thresh, 255, cv2.THRESH_BINARY)
 
 contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 peri = cv2.arcLength(contours[0], True)
-apprsox = cv2.approxPolyDP(contours[0], 0.01*peri, True)
+approx = cv2.approxPolyDP(contours[0], 0.01*peri, True)
 pts = np.float32(approx)
 
-print(pts)
+print(pts[0][0])
 
 # create an empty image for contours
 img_contours = np.zeros(img.shape)
 # draw the contours on the empty image
-cv2.drawContours(img_contours, contours[0], -1, (0, 255, 0), 3)
-# save image
-cv2.imwrite('contours.png', img_contours)
+cv2.drawContours(img, contours[0], -1, (0, 255, 0), 10)
+cv2.drawContours(img_contours, contours[0], -1, (0, 255, 0), 10)
+
+# draw square points
+img[approx[0][0][1],approx[0][0][0]]=(0,0,0)
+img[approx[1][0][1],approx[1][0][0]]=(0,0,0)
+img[approx[2][0][1],approx[2][0][0]]=(0,0,0)
+img[approx[3][0][1],approx[3][0][0]]=(0,0,0)
+
+# print image
+cv2.imshow('contours con imagen', img)
+
+
+cv2.waitKey(0)
+
